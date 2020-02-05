d5c2f61a3d93fb79ae1ef7fcb7ca345b5a773e5d
igamberdievhasan <49599902+igamberdievhasan@users.noreply.github.com>
2020-02-04 22:58:44 -0500

    This android program has a button in the center and a text view above it. when the button is clicked the text view changes from "Hello, World. This is my first app!" to "You have clicked the button".

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView string = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                string.setText("You have clicked the button");
+            }
+        });
+
+
+
     }
 }
